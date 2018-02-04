695b76e49670e605f7f222cbbfe73ced215bef16
tinaoberoi <tinaoberoi70@gmail.com>
2018-02-04 19:06:17 +0530

    facebook sdk configured

--- spamScout/boards/views.py
+++ spamScout/boards/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+import facebook
+
+graph = facebook.GraphAPI(access_token= "EAACEdEose0cBAIzaLNu4P3ddhUWBjoNbTwzieolzrhJnxubWQlWLnxbJyPfurRkbuOLKdbGc3pavcmGWv3dpRocZBbMpCTu97DNHcvEISJXzEJB7wAM8GvGLVh1fvPjXQRxf9YwqEg8eWC6Ms0zDWpjTwK0Fo7gjoQ3KUPZBg3D1MyCsaNlbpQOiW2qS3Bqk6t7KouNgZDZD", version="2.11")
+friends = graph.get_connections(id='me', connection_name='friends')
 
 def home(request):
-    return HttpResponse('spam boii!!');
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
--- spamScout/spamScout/settings.py
+++ spamScout/spamScout/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'spamScout.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  os.path.join(os.path.dirname(__file__) , '../templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
