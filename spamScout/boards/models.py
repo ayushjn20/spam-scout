6cb837f48d940d5b58a86dab47ffdc8acdb0ed1f
tinaoberoi <tinaoberoi70@gmail.com>
2018-02-07 00:36:39 +0530

    django db queires initiated

--- spamScout/boards/models.py
+++ spamScout/boards/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+#from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
+#    UniqueIdProperty, RelationshipTo, RelationshipFrom)
+
+#config.DATABASE_URL = 'bolt://neo4j:ratina@localhost:7687'
+
+import facebook
+import json
+
+class CurrentList(models.Model):
+
+      key = models.IntegerField(default="0")
+      name = models.CharField(max_length=30)
+
+      def __str__(self):
+          return self.name
+"""
+@receiver(post_save, sender=CurrentList)
+def current(sender, instance, **kwargs):
+    
+    if instance is not None:
+       graph = neo4j.Graph('http://localhost:7474/db/data')
+       query = '''
+       CREATE (n:CurrentList {name: {name}})
+       '''
+       graph.cypher.execute(query, name=instance.name)
+
+#class Trustlist(models.Model):
+#      key = models.IntegerField(default="0")
+#      name = models.CharField(max_length=30)
+
+#      def __str__(self):
+#          return self.name
+"""
--- spamScout/boards/views.py
+++ spamScout/boards/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from boards.models import *
+from django.views.decorators.csrf import csrf_exempt
 import facebook
+import json
 
-graph = facebook.GraphAPI(access_token= "EAACEdEose0cBAIzaLNu4P3ddhUWBjoNbTwzieolzrhJnxubWQlWLnxbJyPfurRkbuOLKdbGc3pavcmGWv3dpRocZBbMpCTu97DNHcvEISJXzEJB7wAM8GvGLVh1fvPjXQRxf9YwqEg8eWC6Ms0zDWpjTwK0Fo7gjoQ3KUPZBg3D1MyCsaNlbpQOiW2qS3Bqk6t7KouNgZDZD", version="2.11")
+#user_access_token
+token="EAACEdEose0cBAH9PGHosKww8SArVtsxsFQQnDO4rZBcKBzSigXVy8GWoMkQId7h4svtdy6ANibATJPSHDDZCewgGcojwGl0ELX0gZCQZAMmx2rLOsTrwl5DrhPmcdZAuAzxS3lzoMljLqPH5Emb7fINl0Uxn0rnZAYvixZAtOujoDWGT3UMRo4OB2BxIM1n1FQZD"
+graph = facebook.GraphAPI(access_token= token, version="2.11")
+
+#this will return a dictionary of friends
 friends = graph.get_connections(id='me', connection_name='friends')
+friendList = friends['data']
+print(type(friendList))
+print(friendList[0].keys())  
+ 
+#friends = graph.get_object("me/friends")
+#write in trusted circle list
+#graph.put_object
 
+@csrf_exempt
 def home(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+#    template = loader.get_template('index.html')
+#    return HttpResponse(template.render())\
+     if request.method == 'POST':
+         print(request.POST.keys())
+         try:
+             if(request.POST['id'] in [ d['id'] for d in friendList]):
+               currentList=CurrentList(name=request.POST['name'], key=request.POST['id'])
+               currentList.save()
+               return HttpResponse("Success1!")
+         except Exception as e:
+            print(e)
+            return HttpResponse("Failure1!")
+
+     elif request.method == 'GET':
+        return HttpResponse(json.dumps(friendList))
+
