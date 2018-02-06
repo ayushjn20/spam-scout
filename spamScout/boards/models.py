2d09b1a2d0d3080238431ee350c72ac71cdba032
Tina Oberoi <tinaoberoi70@gmail.com>
2018-02-07 02:47:50 +0530

    basic structure of views

--- spamScout/boards/models.py
+++ spamScout/boards/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -12,13 +12,25 @@ from django.dispatch import receiver
 import facebook
 import json
 
+class CustomUser(models.Model):
+  user = models.OneToOne(User)
+  fb_id = models.CharField(max_length=30)
+  spam_list = models.OneToMany(Spams)
+  fb_token = models.CharField(max_length=500)
+
+
 class CurrentList(models.Model):
+  key = models.IntegerField(default="0")
+  name = models.CharField(max_length=30)
 
-      key = models.IntegerField(default="0")
-      name = models.CharField(max_length=30)
+  def __str__(self):
+    return self.name
 
-      def __str__(self):
-          return self.name
+
+class Spam(models.Model)
+  domain = models.UUIDField(default=uuid.uuid4) 
+
+
 """
 @receiver(post_save, sender=CurrentList)
 def current(sender, instance, **kwargs):
--- spamScout/boards/views.py
+++ spamScout/boards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from boards.models import *
 from django.views.decorators.csrf import csrf_exempt
@@ -8,7 +8,7 @@ import json
 
 #user_access_token
 token="EAACEdEose0cBAH9PGHosKww8SArVtsxsFQQnDO4rZBcKBzSigXVy8GWoMkQId7h4svtdy6ANibATJPSHDDZCewgGcojwGl0ELX0gZCQZAMmx2rLOsTrwl5DrhPmcdZAuAzxS3lzoMljLqPH5Emb7fINl0Uxn0rnZAYvixZAtOujoDWGT3UMRo4OB2BxIM1n1FQZD"
-graph = facebook.GraphAPI(access_token= token, version="2.11")
+graph = facebook.GraphAPI(access_token= token, version="2.1")
 
 #this will return a dictionary of friends
 friends = graph.get_connections(id='me', connection_name='friends')
@@ -24,17 +24,58 @@ print(friendList[0].keys())
 def home(request):
 #    template = loader.get_template('index.html')
 #    return HttpResponse(template.render())\
-     if request.method == 'POST':
-         print(request.POST.keys())
-         try:
-             if(request.POST['id'] in [ d['id'] for d in friendList]):
-               currentList=CurrentList(name=request.POST['name'], key=request.POST['id'])
-               currentList.save()
-               return HttpResponse("Success1!")
-         except Exception as e:
-            print(e)
-            return HttpResponse("Failure1!")
+  if request.method == 'POST':
+    print(request.POST.keys())
+    try:
+      if(request.POST['id'] in [ d['id'] for d in friendList ] ):
+        
+        #Node creation
+        """
+        create_graph_connection(\
+          {'name':(request.user.first_name + request.user.last_name),\
+          'fb_id':request.user.facebook.fb_id}, \
+          {'name': request.POST['name'],\
+          'fb_id': request.POST['id']})
+        """
+        return HttpResponse("Success1!")
+    
+    except Exception as e:
+      print(e)
+      return HttpResponse("Failure1!")
+  elif request.method == 'GET': 
+    
+    return HttpResponse(json.dumps(friendList))
 
-     elif request.method == 'GET':
-        return HttpResponse(json.dumps(friendList))
 
+@csrf_exempt
+def rem_trust(request):
+  if request.method == 'POST':
+    print(request.POST.keys())
+
+    #remove_connection()
+    return HttpResponse("")
+  else 
+
+
+@csrf_exempt
+def add_spam(request):
+  if request.method == 'POST':
+    print(request.POST.keys())
+    try:
+      Spam.objects.get_or_create(domain=request.POST['domain'])
+    except KeyError:
+      return HttpResponse("Invalid request")
+    except Exception as e:
+      return HttpResponse("")
+
+
+@csrf_exempt
+def rem_spam(request):
+  if request.method == 'POST':
+    print(request.POST.keys())
+    try:
+      Spam.objects.get_or_create(domain=request.POST['domain'])
+    except KeyError:
+      return HttpResponse("Invalid request")
+    except Exception as e:
+      return HttpResponse("")
