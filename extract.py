d94f1e81d8f9acfc3071203013c8e0bdd143a807
Ayush Jain <ayushjain20n@gmail.com>
2018-02-07 22:17:00 +0530

    complete extract.py

--- extract.py
+++ extract.py
@@ -1,6 +1,12 @@
 from ipaddress import ip_address
-from urllib.parse import urlparse
-import requests
+from datetime import datetime
+import requests, ast
+from keys import whois_api_key
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 STD_PORTS = [21,22,23,80,443,445,1433,1521,3306,3389]
 
@@ -20,15 +26,15 @@ def extract_features_from_url(url_string):
             final_dict['having_IP_Address'] = 1
     except ValueError:
         final_dict['having_IP_Address'] = -1
-    except:
+    except Exception as e:
         final_dict['having_IP_Address'] = 0
 
     # URL_length
     if len(url_string < 54): final_dict['URL_length'] = 1
-    if len(url_string >= 54 and url_string <= 75): final_dict['URL_length'] = 0
-    if len(url_string > 75): final_dict['URL_length'] = -1
-     #shortining service
+    elif len(54 <= url_string <= 75): final_dict['URL_length'] = 0
+    elif len(url_string > 75): final_dict['URL_length'] = -1
 
+    #Shortining Service
     if (len(domain) < 6):
         final_dict['Shortining_Service'] = 1
     else: final_dict['Shortining_Service'] = 0
@@ -41,23 +47,105 @@ def extract_features_from_url(url_string):
         final_dict['double_slash_redirecting'] = 1
     else: final_dict['double_slash_redirecting'] = -1
 
-    if('-' in domain): final_dict['Prefix-Suffix'] = 1
-    else: final_dict['Prefix-Suffix'] = -1
+    if('-' in domain): final_dict['Prefix_Suffix'] = 1
+    else: final_dict['Prefix_Suffix'] = -1
 
     if (domain.count('.') <= 1): final_dict['having_Sub_domain'] = -1
     elif (domain.count('.') == 2): final_dict['having_Sub_domain'] = 0
     else: final_dict['having_Sub_domain'] = 1
 
-    if(url.scheme=='http'):
-        final_dict['SSLfinal_State'] = 1
+    # if(url.scheme=='http'):
+    #     final_dict['SSLfinal_State'] = 1
+    # else:
+    #     try:
+    #         fetch = requests.get(url_string)
+    #         if (fetch.status_code == 200): final_dict['SSLfinal_State'] = -1
+    #     except:
+    #         final_dict['SSLfinal_State'] = 0
+
+    final_dict['port'] = 1
+    if(port):
+        if(port not in STD_PORTS):
+            final_dict['port'] = -1
+
+    if (url.scheme == 'http'):
+        final_dict['HTTPS_token'] = 1
     else:
         try:
             fetch = requests.get(url_string)
-            if (fetch.status_code == 200): final_dict['SSLfinal_State'] = -1
+            if (fetch.status_code == 200): final_dict['HTTPS_token'] = -1
         except:
-            final_dict['SSLfinal_State'] = 0
+            final_dict['HTTPS_token'] = 0
 
-    if(port):
-        if(port in STD_PORTS):
-            final_dict['port']
-    else: pass
+    try:
+        from urllib.request import urlopen
+    except ImportError:
+        from urllib2 import urlopen
+
+    apiKey = whois_api_key
+
+    x = 0
+    for url in chase_redirects(url.scheme + '://' + url.netloc):
+        print(url)
+        x += 1
+    if (x <= 1):
+        final_dict['Redirect'] = -1
+    elif (x >= 2 and x < 4):
+        final_dict['Redirect'] = 0
+    else:
+        final_dict['Redirect'] = 1
+
+
+    url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?' \
+          + 'domainName=' + domain + '&apiKey=' + apiKey + "&outputFormat=JSON"
+    try:
+        whois_res = ast.literal_eval(urlopen(url).read().decode('utf8')) # a dict
+
+        hostnames = [hn.lower() for hn in whois_res['WhoisRecord']['registryData']['nameServers']['hostNames']]
+        final_dict['Abnormal_URL'] = 1
+        for hn in hostnames:
+            if(hn.contains(domain)):
+                final_dict['Abnormal_URL'] = -1
+                break
+
+        start_date = whois_res['WhoisRecord']['registryData']['createdDate']
+        end_date = whois_res['WhoisRecord']['registryData']['expiresDate']
+
+        def __datetime(date_str):
+            return datetime.strptime(date_str, '%a %b %d %H:%M:%S +0000 %Y')
+
+        start = __datetime(start_date)
+        end = __datetime(end_date)
+        delta_months = int((end - start).total_seconds() * 3.80517e-7)
+
+        if delta_months < 6:
+            final_dict['age_of_domain'] = 1
+        else:
+            final_dict['age_of_domain'] = -1
+
+    except Exception as e:
+        final_dict['Abnormal_URL'] = 0
+        final_dict['age_of_domain'] = 0
+
+
+
+def chase_redirects(url):
+    while True:
+        yield url
+        r = requests.head(url)
+        if 300 < r.status_code < 400:
+            url = r.headers['location']
+        else:
+            break
+
+
+def extract_from_links(data, url_string):
+    url = urlparse(url_string)
+    domain = url.netloc.split(':')[0]
+
+    dicct = {}
+
+    for key, list in data:
+        dicct[key] = list.count(domain)
+
+    return dicct
---
+++ keys.py
@@ -0,0 +1,1 @@
+whois_api_key = "at_Usr6RQFC6oji42PXIy2JO5TAPMcho"
